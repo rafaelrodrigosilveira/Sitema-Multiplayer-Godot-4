a6c0d43c7d0fc3a0328f5cf05a9af89e40927164
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-04-17 19:31:15 -0300

    v0.10
    reduzido a resolução de:
    1152x648
    Para: 800x600

--- lan.gd
+++ lan.gd
@@ -5,6 +5,7 @@ extends Control
 func _ready():
 	Network.lista_alterada.connect(self.lista_alterada)
 	Network.conexao_resetada.connect(self.conexao_resetada)
+	$"Começar".disabled = true
 	pass
 
 
@@ -15,6 +16,7 @@ func _on_criar_pressed():
 	$Conectar.disabled = true # desabilita botão
 	var ip = Network.retornar_ip()
 	$IPServidor.text = "IP do Servidor: " + ip
+	$"Começar".disabled = false
 	pass 
 
 
@@ -24,6 +26,7 @@ func _on_conectar_pressed():
 	Network.entrar_servidor()
 	$Criar.disabled = true # desabilita botão
 	$Conectar.disabled = true # desabilita botão
+	#$"Começar".disabled = false
 	pass 
 
 func _on_começar_pressed():
@@ -50,7 +53,7 @@ func conexao_resetada():
 	$ErroPanel.show()
 	pass
 
-
+# Tratamento de erro
 func _on_erropanel_button_pressed():
 	$Criar.disabled = false # desabilita botão
 	$Conectar.disabled = false # desabilita botão
